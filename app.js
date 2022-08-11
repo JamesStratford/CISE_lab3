d3037d3001215c3fd0a90183688b2b06f0caec85
JamesStratford <james.e.stratford@gmail.com>
2022-08-11 21:07:30 +1200

    -

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const connectDB = require('./config/db');
 var cors = require('cors');
 
 // routes
-const books = require('./routes/api/books');
+const books = require('./routes/api/books.js');
 
 const app = express();
+const port = process.env.PORT || 8082;
 
+app.listen(port, () => console.log(`Server running on port ${port}`));
 // Connect Database
 connectDB();
 
@@ -18,25 +20,22 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello world!'));
-
 
 
 // Accessing the path module ---- Heroku deployment
+// Step 1:
 const path = require("path");
 
-// Step 1:
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.resolve(__dirname, "./my-app/build")));
+if (process.env.NODE_ENV === "production") {
 
-  app.get("*", function (request, response) {
-    response.sendFile(path.resolve(__dirname, "./my-app/build", "index.html"));
-  });
-}
-else {
-  app.get("/", (req, res) => {
-    res.send("Api Running");
-  })
+    app.use(express.static("my-app/build"));
+
+    app.get("*", (req, res) => {
+
+    res.sendFile(path.resolve(__dirname, "my-app", "build", "index.html"));
+
+   });
+
 }
 // Step 2:
 // ------------------------------------------------
@@ -45,6 +44,3 @@ else {
 // use Routes
 app.use('/api/books', books);
 
-const port = process.env.PORT || 8082;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
